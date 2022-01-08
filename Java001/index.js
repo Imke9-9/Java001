c9e536cbcca538c55319ba2ab3ebf7b69951f9e6
Imke9-9 <94930959+Imke9-9@users.noreply.github.com>
2022-01-08 16:28:23 +0100

    add files upload

---
+++ Java001/index.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+
+let name = 'Imke';
+console.log (name);
+
+let som = 4 + 4
+console.log (som)
+
+let som2 = '4'+'4'
+console.log (som2)
+
+let age = '29'
+console.log (age)
